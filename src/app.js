25d3068b92791754ad78dffc9d6ac42aebed7bfd
nmacenco <nicolasmacenco@gmail.com>
2021-09-01 20:01:21 -0300

    Eliminada la carpeta node_modules del repositorio

---
+++ src/app.js
@@ -0,0 +1,46 @@
+//////////// REQUIRES //////////////////
+const express = require ("express") ;
+const path = require ('path') ;
+const app = express() ;
+const methodOverride = require ('method-override') ; 
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+
+
+///////////// MIDDLEWARES //////////////////////
+app.use (express.static('public'));
+app.use (methodOverride('_method')) ;
+app.use(cookieParser());
+app.use(express.urlencoded({ extended: false }));
+app.use(logger('dev'));
+app.use(express.json());
+
+/////////// TEMPLATE ENGINE //////////////
+app.set ('view engine' , 'ejs') ;   
+app.set ('views' , path.join(__dirname, './src/views') ) ;
+
+
+
+app.listen (process.env.PORT || 3000, () => console.log('El servidor se esta ejecutando en http://localhost:3000') ) ;
+
+
+/* REQUIRES DE ARCHIVOS DE RUTAS */
+
+const rutasHome = require ('./routes/main');
+const rutasProducts = require ('./routes/products');
+const rutasUsers = require ('./routes/users');
+
+
+/* DEFINICIÓN DE RUTAS EN LA WEB */
+
+app.use ('/' , rutasHome)
+app.use ('/users' , rutasUsers)
+app.use ('/products' , rutasProducts)
+
+
+/* 404 */
+
+app.use((req,res,next) => {
+    res.status(404).render('404');
+    next();
+});
