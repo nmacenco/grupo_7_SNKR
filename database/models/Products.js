d3773ef23711ec00fa02dc6fb8461801d2dda334
nmacenco <nicolasmacenco@gmail.com>
2021-10-19 11:35:48 -0300

    Instalacion de sequelize y configuracion del modelo Products

---
+++ database/models/Products.js
@@ -0,0 +1,50 @@
+module.exports = (sequelize , dataTypes) => {
+    let alias = 'Products' ;
+    let cols = { 
+        id : {
+            type : dataTypes.INTEGER , 
+            primaryKey : true , 
+            autoIncrement : true, 
+            allownNull : false
+        },
+        name : { 
+            type : dataTypes.STRING(100) ,
+            allownNull : false 
+        },
+        price : { 
+            type : dataTypes.DECIMAL(10,2) ,
+            allownNull : false 
+        },
+        detail : { 
+            type : dataTypes.STRING(8000),
+            allownNull : true , 
+            defaultValue : null 
+        },
+        gender : { 
+            type : dataTypes.STRING(100),
+            allownNull : false 
+        }, 
+        brand : { 
+            type : dataTypes.STRING(100), 
+            allownNull : false 
+        },
+        category : { 
+            type : dataTypes.STRING(100),
+            allownNull : false 
+        }, 
+        image : { 
+            type : dataTypes.STRING(100),
+            allownNull : false 
+        }
+
+    } ;
+    let config = { 
+        tableName : 'products' ,
+        timestamps : false 
+    } ;
+
+
+    const Products = sequelize.define(alias,cols,config) ;
+
+    return Products 
+}
