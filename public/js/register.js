0826edcbbff10e8cb7967f05cead511ae134be9d
mauriciosilvera <mauricioagustin_@outlook.com>
2021-11-14 20:52:36 -0300

    Terminada la validación front-end de registro y login completos, avances en el backend de creacion/modificacion de productos.

--- public/js/register.js
+++ public/js/register.js
@@ -8,7 +8,7 @@ let inputs = document.querySelectorAll('.register input') ;
 // CAPTURO INPUTS DEL FORMULARIO
 let usuario = document.getElementById('usuario');
 let password = document.getElementById('password');
-let confirmacionPassword = document.getElementById('confirmacionPassword');
+let confirmacionPassword = document.getElementById('rep_password');
 let nombre = document.getElementById('nombre');
 let apellido = document.getElementById('apellido');
 let email = document.getElementById('email');
@@ -16,7 +16,28 @@ let email = document.getElementById('email');
 let image = document.querySelector('avatar') ;
 
 
-//VALIDACIONES DE DATOS 
+// VALIDACIONES DE DATOS 
+
+usuario.addEventListener('blur', function(){
+
+    console.log(usuario);
+    usuarioSpan = document.querySelector('.usuarioErr');
+    
+    if(usuario.value == '')
+    {
+        usuarioSpan.innerHTML = "Por favor ingrese el usuario."
+        usuario.classList.remove('is-ok');
+        usuario.classList.add('alert-err');
+    }
+    else
+    {
+        usuarioSpan.innerHTML =  '' ;
+        usuario.classList.remove('alert-err');
+        usuario.classList.add('is-ok');
+    }
+
+})
+
 nombre.addEventListener('blur', function(){
 
     nombreSpan = document.querySelector('.nombreErr');
@@ -37,6 +58,26 @@ nombre.addEventListener('blur', function(){
 
 })
 
+apellido.addEventListener('blur', function(){
+
+    apellidoSpan = document.querySelector('.apellidoErr');
+    
+    if(apellido.value.length <= 2)
+    {
+        apellidoSpan.innerHTML = "El apellido debe tener al menos 2 caracteres."
+        apellido.classList.remove('is-ok');
+        apellido.classList.add('alert-err');
+        
+    }
+    else
+    {
+       apellidoSpan.innerHTML =  '' ;
+        apellido.classList.remove('alert-err');
+        apellido.classList.add('is-ok');
+    }
+
+})
+
 password.addEventListener('blur', function(){
 
     passwordSpan = document.querySelector('.passwordErr');
@@ -53,6 +94,26 @@ password.addEventListener('blur', function(){
         password.classList.remove('alert-err');
         password.classList.add('is-ok');
     }
+})
+
+confirmacionPassword.addEventListener('blur', function(){
+
+    confirmacionPasswordSpan = document.querySelector('.confirmacionPasswordErr');
+    console.log(confirmacionPassword.value)
+    console.log(password.value)
+    if(confirmacionPassword.value !== password.value)
+    {
+        confirmacionPasswordSpan.innerHTML = "Las contraseñas no coinciden."
+        confirmacionPassword.classList.remove('is-ok');
+        confirmacionPassword.classList.add('alert-err');
+    }
+    else
+    {
+        confirmacionPasswordSpan.innerHTML =  '' ;
+        confirmacionPassword.classList.remove('alert-err');
+        confirmacionPassword.classList.add('is-ok');
+    }
+
 })
 
 email.addEventListener('blur', function(){
--- src/routes/products.js
+++ src/routes/products.js
@@ -1,11 +1,11 @@
-//  REQUIRES    //
-const express = require ('express') ;
-const router = express.Router () ;
-const path = require ('path') ;
-const multer = require ('multer')
+////////////////      REQUIRES        //////////////////////////////
+const express = require('express');
+const router = express.Router();
+const path = require ('path');
+const multer = require('multer');
+const { body, check } = require('express-validator');
 
-//  CONFIGURACION DE MULTER //
-
+////////////////     CONFIGURACION DE MULTER      //////////////////
 const storage = multer.diskStorage ({
     destination: (req,file,cb) => {
         folder = path.join(__dirname, '../../public/images/products')
@@ -22,9 +22,58 @@ const upLoadFile = multer({storage});
 const productsControllers = require ('../controllers/productsControllers')
 
 
+/////////////////       EXPRESS VALIDATOR       ///////////////
+
+// //Validación CUSTOM para las imagenes, ya que express validator solo valida strings.
+// function validacionImagenes (archivo){
+//     let extension = (path.extname(archivo)).toLowerCase();
+
+//     switch(extension) {
+//         case '.jpg': 
+//             return 'ok'; 
+//             break;
+//         case '.jpeg': 
+//             return 'ok'; 
+//             break;
+//         case '.png': 
+//             return 'ok'; 
+//             break;
+//         case '.gif': 
+//             return 'ok'; 
+//             break;
+
+//         default: return false
+//     }
+// }
+
+let validacionesEditProducts = [
+    body('name')
+        .notEmpty().withMessage('Por favor ingresa el nombre del producto.')
+        .isLength({min:5, max: undefined}).withMessage('El nombre del producto debe tener como mínimo 5 caracteres.'),
+    body('detail')
+        .notEmpty().withMessage('Por favor ingresa una descripción del producto').bail()
+        .isLength({min:20, max:undefined}).withMessage('La descripción debe tener como mínimo 20 caracteres.'),
+    body('image').custom((value, { req }) => {
+        let file = req.file;
+        let acceptedExtensions = ['.jpg', '.png', '.gif'];
+        
+        if (!file) {
+            throw new Error('Tienes que subir una imagen');
+        } else {
+            let fileExtension = path.extname(file.originalname);
+            if (!acceptedExtensions.includes(fileExtension)) {
+                throw new Error(`Las extensiones de archivo permitidas son ${acceptedExtensions.join(', ')}`);
+            }
+        }
+
+        return true;})
+];
+
+
+/////////////////////////       RUTAS       ////////////////////////// 
 //  FORMULARIO DE CREACION DE PRODUCTOS //
 router.get ('/create' , productsControllers.create) 
-router.post ('/new/' , upLoadFile.single('image') , productsControllers.store) 
+router.post ('/new/' , upLoadFile.single('image'), productsControllers.store) 
 
 // CARRITO DE PRODUCTOS //
 router.get ('/cart', productsControllers.cart);
@@ -38,10 +87,8 @@ router.get ('/:id', productsControllers.detail);
 //  LISTADO GENERAL DE PRODUCTOS    //
 router.get('/', productsControllers.list);
 
-
-
 //  FORMULARIO DE EDICION DE PRODUCTOS  //
-router.get ('/edit/:id' , productsControllers.edit); 
+router.get ('/edit/:id' , validacionesEditProducts, productsControllers.edit); 
 
 //  ACCION DE EDITAR PRODUCTO  //
 router.put('/edit/:id' ,upLoadFile.single('image'),  productsControllers.update);
